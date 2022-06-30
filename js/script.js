1e753056e5c8ad470aa825824f4b293d5b44d90d
Mario Albano <marioalbanon@gmail.com>
2022-06-30 10:52:23 -0300

    Resolvido problema com a lógica contra IA

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let x = document.querySelector(".x");
 let o = document.querySelector(".o");
 let boxes = document.querySelectorAll(".box");
-let buttons = document.querySelectorAll("buttons-container button");
+let buttons = document.querySelectorAll("#buttons-container button");
 let messageContainer = document.querySelector("#message");
 let messageText = document.querySelector("#message p");
 let secondPlayer;
@@ -27,8 +27,16 @@ for (let i = 0; i < boxes.length; i++) {
             //computando a jogada
             if (player1 == player2) {
                 player1++;
-            }
-            else {
+
+                if (secondPlayer == "ai-player") {
+
+                    //Funcao executar a jogada
+                    computerPlay();
+
+                    player2++;
+                }
+
+            } else {
                 player2++;
             }
 
@@ -36,6 +44,25 @@ for (let i = 0; i < boxes.length; i++) {
             checkWinCondition();
 
         }
+    });
+}
+
+//Evento para saber se é 2 players ou contra CPU
+for (let i = 0; i < buttons.length; i++) {
+
+    buttons[i].addEventListener("click", function () {
+
+        secondPlayer = this.getAttribute("id");
+
+        for (let j = 0; j < buttons.length; j++) {
+            buttons[j].style.display = "none";
+        }
+
+        setTimeout(function () {
+            let container = document.querySelector("#container");
+            container.classList.remove("hide");
+        }, 500);
+
     });
 }
 
@@ -226,3 +253,30 @@ function declareWinner(winner) {
     }
 }
 
+// Executar a lógica da jogada do CPU
+function computerPlay() {
+    let cloneO = o.cloneNode(true);
+    counter = 0;
+    filled = 0;
+
+    for (let i = 0; i < boxes.length; i++) {
+
+        let randomNumber = Math.floor(Math.random() * 5);
+
+        //Preenchendo somente se o filho estiver sozinho
+        if (boxes[i].childNodes[0] == undefined) {
+            if (randomNumber <= 1) {
+                boxes[i].appendChild(cloneO);
+                counter++;
+                break;
+            }
+
+            //Checagem de quantas estão preenchidas
+        } else {
+            filled++;
+        }
+    }
+    if (counter == 0 && filled < 9) {
+        computerPlay();
+    }
+}
